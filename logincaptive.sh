887110df0fee84177ac7f6bf6c512aa1dd0e3ac5
Mário Jorge Santos <mariojsantos@users.noreply.github.com>
2023-08-24 22:23:55 -0300

    Some necessary changes
    1. Added GPL License
    2. Added generic arguments to url

--- logincaptive.sh
+++ logincaptive.sh
@@ -5,13 +5,30 @@
 # Assign permissions on the file just to the file owner:
 # chmod 600 .pass
 # Be happy!
+##################################################################################### 
+#
+#   This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+#################################################################################### 
 
 PASS="`<.pass`"
 
 #Put your captive portal username here between the double quotes
 USERNAME="<yourusername>"
 
-RESULT="$(curl -s -k -H "Content-Type: application/x-www-form-urlencoded" -X POST --data-urlencode "auth_user=$USERNAME" --data-urlencode "auth_pass=$PASS" -d accept=Continuar https://acesso.ifcelimoeiro:8007/index.php?zone=institucional_temp)"
+RESULT="$(curl -s -k -H "Content-Type: application/x-www-form-urlencoded" -X POST --data-urlencode "auth_user=$USERNAME" --data-urlencode "auth_pass=$PASS" -d accept=Continuar https://<CaptivePortalIPorDomain>:<Port>/index.php?zone=<ZoneName>)"
 
 YES=`echo $RESULT | grep Redirec`
 
