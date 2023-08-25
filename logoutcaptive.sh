aaf768861c7cc299f2343f4eee4897208a51fea2
Mário Jorge Santos <mariojsantos@users.noreply.github.com>
2023-08-24 22:30:05 -0300

    Some necessary changes
    Added GPL License
    Added generic arguments to urls

--- logoutcaptive.sh
+++ logoutcaptive.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-# Script para fazer logout no captive portal
-# 
-# Atribua permissões somente ao dono do arquivo:
+# Script to logout from captive portal
+# Assign permitions just to the file owner
 # chmod 700 logoutcaptive.sh
-# Seja feliz!
+# Be happy!
+##################################################################################### 
+#
+#   This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+#################################################################################### 
 
-ID=`curl -s -k https://acesso.ifcelimoeiro:8007/index.php?zone=institucional_temp | grep logout_id | cut -d" " -f4 | cut -d= -f2 | sed 's/"//g'`  
+ID=`curl -s -k https://<CaptivePortalIPorDomain>:<Port>/index.php?zone=<ZoneName> | grep logout_id | cut -d" " -f4 | cut -d= -f2 | sed 's/"//g'`  
 
-curl -k -X POST -F 'logout_id='$ID'' -F 'zone=institucional_temp' -F 'logout=Disconnect' https://acesso.ifcelimoeiro:8007/index.php?zone=institucional_temp
+curl -k -X POST -F 'logout_id='$ID'' -F 'zone=institucional_temp' -F 'logout=Disconnect' https://<CaptivePortalIPorDomain>:<Port>/index.php?zone=<ZoneName>
 
 echo Logout Successful!
